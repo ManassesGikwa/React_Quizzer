81f98123ada259e4cd3c8412709738730206a4b8
Abdirahman <abdirahman.abdi01@student.moringaschool.com>
2024-02-14 17:52:48 +0300

    new commits app DifficultSelector Question and Result

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import DifficultySelector from './components/DifficultySelector';
+import Questions from './components/Questions';
+import Result from './components/Result';
 
 function App() {
+  const [difficulty, setDifficulty] = useState('');
+  const [questions, setQuestions] = useState([]);
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [quizFinished, setQuizFinished] = useState(false);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await fetch('https://react-quizer-vercel.vercel.app/questions');
+        const data = await response.json();
+        const filteredQuestions = data.filter(question => question.difficulty === difficulty);
+        setQuestions(filteredQuestions);
+        setQuizStarted(true);
+      } catch (error) {
+        console.error('Error fetching questions:', error);
+      }
+    };
+
+    if (quizStarted && questions.length === 0) {
+      fetchQuestions();
+    }
+  }, [difficulty, quizStarted, questions]);
+
+  const onStartQuiz = () => {
+    if (difficulty) {
+      setQuizStarted(true);
+    }
+  };
+
+  const onFinishQuiz = () => {
+    setQuizFinished(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Quizzer</h1>
+      <h2>Choose Difficulty:</h2>
+      <DifficultySelector setDifficulty={setDifficulty} />
+      {difficulty && !quizStarted && (
+        <button onClick={onStartQuiz}>Start Quiz</button>
+      )}
+      {quizStarted && questions.length > 0 && !quizFinished && (
+        <Questions questions={questions} onFinishQuiz={onFinishQuiz} />
+      )}
+      {quizFinished && <Result />}
     </div>
   );
 }
