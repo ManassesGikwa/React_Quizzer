f1b1f3563f9906c08fc926baed06d0152bcd0c75
111111-glitc <ngigimelissa@gmail.com>
2024-02-15 11:48:53 +0300

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 
->>>>>>> Stashed changes
 import Home from './components/Home';
 import Result from './components/Result';
 import DifficultySelector from './components/DifficultySelector';
