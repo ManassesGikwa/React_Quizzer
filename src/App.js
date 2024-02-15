c6faa55831347d019330353380f23cbcf32e4f9f
manoti3 <hillarymanoti0@gmail.com>
2024-02-15 13:36:26 +0300

    Merge branch 'development' into your-branch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-=======
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import Quiz from './Quiz';
-import Result from './Result';
-import DifficultySelector from './DifficultySelector';
-import Questions from './Questions'; 
->>>>>>> 5f11b3ebd01f69954b2ad861d5777c397789ab60
 
 import Home from './components/Home';
 import Questions from './components/Questions';
