9c50bdf8cc24e7dc3c66de7e9478259240d02e9e
Abdirahman <abdirahman.abdi01@student.moringaschool.com>
2024-02-16 06:57:32 +0300

    updated app.css and home.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/questions" element={<Questions />} />
         <Route path="/results" element={<Results />} />
-        <Route path="/difficulty" element={<DifficultySelector />} /> {/* Add route for DifficultySelector */}
+        <Route path="/difficulty" element={<DifficultySelector />} />
       </Routes>
     </Router>
   );
